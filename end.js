d563d93deb1677e77608224817f5ed3f43ae58c6
jpk3lly <39955081+jpk3lly@users.noreply.github.com>
2020-03-31 00:45:27 +0100

    Update end.js

--- end.js
+++ end.js
@@ -26,5 +26,5 @@ saveHighScore = e => {
     highScores.splice(5);
 
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    window.location.assign("//C:/Users/jpk3l/source/repos/Quiz/Quiz/index.html");
+    window.location.assign("index.html");
 };
