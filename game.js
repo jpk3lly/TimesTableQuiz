fcec8603639d188f9f853065d94fd20b7181cdc4
jpk3lly <39955081+jpk3lly@users.noreply.github.com>
2020-08-03 23:49:06 +0100

    Update game.js

--- game.js
+++ game.js
@@ -13,7 +13,7 @@ let availableQuesions = [];
 
 let questions = [];
 
-fetch("TimesTablesQuestions.json", { mode: 'no-cors'})
+get("TimesTablesQuestions", { mode: 'no-cors'})
     .then(res => {
         return res.json();
     })
