0ae94b0795601e794a0cc30751a414e6f65ae940
jpk3lly <39955081+jpk3lly@users.noreply.github.com>
2021-10-01 17:27:34 +0100

    Update game.js

--- game.js
+++ game.js
@@ -13,7 +13,9 @@ let availableQuesions = [];
 
 let questions = [];
 
-fetch("https://jpk3lly.github.io/TimesTablesAPI/TimesTables.json")
+//https://jpk3lly.github.io/TimesTablesAPI/TimesTables.json
+
+fetch("https://jpk3lly.github.io/TimesTableQuiz/TimesTablesQuestions.json")
     .then(res => {
         return res.json();
     })
