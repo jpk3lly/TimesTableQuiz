23f33c2d7711677156ea8b9ac616c336c099565e
John-Paul Kelly <jpk3lly@hotmail.com>
2023-10-01 10:51:14 +0100

    Changed when the options screen appears

---
+++ options.js
@@ -0,0 +1,11 @@
+const numberOfQuestionsDropdown = document.getElementById("Select2");
+
+let noOfQuestions;
+
+
+howManyQuestions = () => {
+    noOfQuestions = numberOfQuestionsDropdown;
+    console.log(noOfQuestions.innerText);
+};
+
+howManyQuestions();
